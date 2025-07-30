49dcbffefc1b05faa46b67574f1fb3a0c74ddfc5
Shay Sapozhnikov <45404072+ShaySapozhnikov@users.noreply.github.com>
2025-07-30 12:51:29 +0200

    adding backup

--- ctf-client/lift-off/src/main.jsx
+++ ctf-client/lift-off/src/main.jsx
@@ -7,16 +7,13 @@ import "./index.css";
 import Home from "./pages/home.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Start from "./pages/start.jsx";
+import Backup from "./pages/backup.jsx";
 
 const router = createBrowserRouter([
   {path: "*", element: <NotFound />}, // needs btter ui!!
   { path: "/", element: <Home /> },
   {path:"start" , element: <Start />},
-
-
-
-
-
+  {path: "backup" , element: <Backup />}
 ]);
 
 
