806e3d81a2884b664728cfce7d1f66b379d3bf0f
Shay Sapozhnikov <45404072+ShaySapozhnikov@users.noreply.github.com>
2025-09-13 01:10:45 -0500

    added logo

--- ctf-client/lift-off/src/pages/home.jsx
+++ ctf-client/lift-off/src/pages/home.jsx
@@ -1,24 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import Board from "../components/Board";
 import CountDown from "../components/countDown";
 import TerminalChat from "../components/TerminalChat";
 import ChatLog from "../components/ChatLog";
-import FlagIcon from "../components/FlagIcon";
 
 function Home() {
-  return (
-    <div className="bg-zinc-900 min-h-screen relative pt-20 flex justify-center items-center">
-      {/* Flag icon component */}
-      <FlagIcon />
+  const navigate = useNavigate();
 
-      {/* Main content grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-4 md:grid-rows-2 gap-5 w-full max-w-[1100px] px-4">
-        <Board />
-        <CountDown />
-        <TerminalChat />
-        <ChatLog />
+  const handleLogoClick = () => {
+    navigate('/flags');
+  };
+
+  return (
+    <div className="bg-zinc-900 min-h-screen relative flex flex-col">
+      {/* Header with logo */}
+      <div className="flex justify-center pt-6 pb-4">
+        <img 
+          src="/logo_white_fullname.png" 
+          alt="Logo" 
+          className="h-12 w-auto opacity-90 hover:opacity-100 transition-opacity duration-300 cursor-pointer hover:scale-105 transform"
+          onClick={handleLogoClick}
+        />
       </div>
+      {/* Main content area */}
+      <div className="flex-1 flex justify-center items-center px-4 py-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-4 md:grid-rows-2 gap-6 w-full max-w-[1200px]">
+          <Board />
+          <CountDown />
+          <TerminalChat />
+          <ChatLog />
+        </div>
+      </div>
+
+      {/* Footer spacer to ensure content doesn't touch bottom */}
+      <div className="h-4"></div>
     </div>
   );
 }
